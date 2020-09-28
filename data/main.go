5456cba09a571b40773ff1575a4cd8e41cd02163
Tim Kotowski <tkotowski7@gmail.com>
2020-09-28 09:36:39 -0500

    selection sort done

--- data/main.go
+++ data/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+
 	"godata/data/bubble"
+	"godata/data/selection"
 )
 
 func main() {
-	array := []int{8, 5, 2, 1, 23, 6, 88, 4, 88, 979, 9, 5, 6, 3, 21, 4, 2, 13, 10, 0, 19, 11, 7, 5, 23, 18, 9, 14, 6, 8, 1, 20, 17, 3, 16, 22, 24, 15, 12}
+	array := []int{8, 5, 2, 1, 23, 6, 88, 4, 88, 979, 9, 5, 6, 3}
 	fmt.Println("not sorted", array)
-	bubble.BubbleSort(array)
-	fmt.Println("sortred", array)
+	bubble.BubbleSort(array	)
+	fmt.Println("bublesort sorted", array)
+	arrayTwo := []int{2,4,6,5,3,1}
+	fmt.Println("not sorted", arrayTwo)
+	selection.SelectionSort(arrayTwo)
+	fmt.Println("selectionsort sorted", arrayTwo)
 }
